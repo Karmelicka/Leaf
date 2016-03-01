6155ce708a1edadce5db09775c0e3c0234156958
Zach Brown <zach.brown@destroystokyo.com>
2016-03-01 13:24:16 -0600

    Allow nerfed mobs to jump

--- src/main/java/net/minecraft/world/entity/Mob.java
+++ src/main/java/net/minecraft/world/entity/Mob.java
@@ -113,6 +113,7 @@ public abstract class Mob extends LivingEntity implements Targeting {
     private final BodyRotationControl bodyRotationControl;
     protected PathNavigation navigation;
     public GoalSelector goalSelector;
+    @Nullable public net.minecraft.world.entity.ai.goal.FloatGoal goalFloat; // Paper - Allow nerfed mobs to jump and float
     public GoalSelector targetSelector;
     @Nullable
     private LivingEntity target;
@@ -878,7 +879,15 @@ public abstract class Mob extends LivingEntity implements Targeting {
     @Override
     protected final void serverAiStep() {
         ++this.noActionTime;
-        if (!this.aware) return; // CraftBukkit
+        // Paper start - Allow nerfed mobs to jump and float
+        if (!this.aware) {
+            if (goalFloat != null) {
+                if (goalFloat.canUse()) goalFloat.tick();
+                this.getJumpControl().tick();
+            }
+            return;
+        }
+        // Paper end - Allow nerfed mobs to jump and float
         this.level().getProfiler().push("sensing");
         this.sensing.tick();
         this.level().getProfiler().pop();
