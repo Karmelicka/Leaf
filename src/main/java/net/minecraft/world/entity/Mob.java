ae92b81634acebe773c82fadae67ed1b3a835204
BillyGalbreath <Blake.Galbreath@GMail.com>
2017-05-05 03:57:17 -0500

    Item#canEntityPickup

--- src/main/java/net/minecraft/world/entity/Mob.java
+++ src/main/java/net/minecraft/world/entity/Mob.java
@@ -671,6 +671,11 @@ public abstract class Mob extends LivingEntity implements Targeting {
                 ItemEntity entityitem = (ItemEntity) iterator.next();
 
                 if (!entityitem.isRemoved() && !entityitem.getItem().isEmpty() && !entityitem.hasPickUpDelay() && this.wantsToPickUp(entityitem.getItem())) {
+                    // Paper start - Item#canEntityPickup
+                    if (!entityitem.canMobPickup) {
+                        continue;
+                    }
+                    // Paper end - Item#canEntityPickup
                     this.pickUpItem(entityitem);
                 }
             }
