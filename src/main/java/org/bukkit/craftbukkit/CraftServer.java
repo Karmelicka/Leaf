5d528bf8d4e0045d5a8e6e85588a2d5a263cac96
kashike <kashike@vq.lc>
2017-06-09 07:24:34 -0700

    Add configuration option to prevent player names from being suggested

--- src/main/java/org/bukkit/craftbukkit/CraftServer.java
+++ src/main/java/org/bukkit/craftbukkit/CraftServer.java
@@ -2773,5 +2773,10 @@ public final class CraftServer implements Server {
         commandMap.registerServerAliases();
         return true;
     }
+
+    @Override
+    public boolean suggestPlayerNamesWhenNullTabCompletions() {
+        return io.papermc.paper.configuration.GlobalConfiguration.get().commands.suggestPlayerNamesWhenNullTabCompletions;
+    }
     // Paper end
 }
