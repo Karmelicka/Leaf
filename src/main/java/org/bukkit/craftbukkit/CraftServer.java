29cbe43ba1210bd9e5464282914cdc8241ba09ec
Aikar <aikar@aikar.co>
2020-06-09 03:33:03 -0400

    Add Plugin Tickets to API Chunk Methods
    Like previous versions, plugins loading chunks kept them loaded until
    they garbage collected to avoid constant spamming of chunk loads
    
    This adds tickets to a few more places so that they can be unloaded.
    
    Additionally, this drops their ticket level to BORDER so they wont be ticking
    so they will just sit inactive instead.
    
    Using .loadChunk to keep a chunk ticking was a horrible idea for upstream
    when we have TWO methods that are able to do that already in the API.
    
    Also reduce their collection count down to a maximum of 1 second. Barely
    anyone knows what chunk-gc is in bukkit.yml as its less relevant now, and
    since this wasn't spigot behavior, this is safe to mostly ignore (unless someone
    wants it to collect even faster, they can restore that setting back to 1 instead of 20+)
    
    Not adding it to .getType() though to keep behavior consistent with vanilla for performance reasons.

--- src/main/java/org/bukkit/craftbukkit/CraftServer.java
+++ src/main/java/org/bukkit/craftbukkit/CraftServer.java
@@ -379,7 +379,7 @@ public final class CraftServer implements Server {
         this.overrideSpawnLimits();
         console.autosavePeriod = this.configuration.getInt("ticks-per.autosave");
         this.warningState = WarningState.value(this.configuration.getString("settings.deprecated-verbose"));
-        TicketType.PLUGIN.timeout = this.configuration.getInt("chunk-gc.period-in-ticks");
+        TicketType.PLUGIN.timeout = Math.min(20, this.configuration.getInt("chunk-gc.period-in-ticks")); // Paper - cap plugin loads to 1 second
         this.minimumAPI = this.configuration.getString("settings.minimum-api");
         this.loadIcon();
 
@@ -947,7 +947,7 @@ public final class CraftServer implements Server {
         this.console.setMotd(config.motd);
         this.overrideSpawnLimits();
         this.warningState = WarningState.value(this.configuration.getString("settings.deprecated-verbose"));
-        TicketType.PLUGIN.timeout = this.configuration.getInt("chunk-gc.period-in-ticks");
+        TicketType.PLUGIN.timeout = Math.min(20, configuration.getInt("chunk-gc.period-in-ticks")); // Paper - cap plugin loads to 1 second
         this.minimumAPI = this.configuration.getString("settings.minimum-api");
         this.printSaveWarning = false;
         this.console.autosavePeriod = this.configuration.getInt("ticks-per.autosave");
