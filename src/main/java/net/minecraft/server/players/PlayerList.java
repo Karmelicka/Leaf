2fd80f60f35b6cc67daf9fa51486841b708a9fe1
Jake Potrebic <jake.m.potrebic@gmail.com>
2021-01-04 19:49:15 -0800

    fix converting txt to json file

--- src/main/java/net/minecraft/server/players/PlayerList.java
+++ src/main/java/net/minecraft/server/players/PlayerList.java
@@ -177,6 +177,7 @@ public abstract class PlayerList {
         this.maxPlayers = maxPlayers;
         this.playerIo = saveHandler;
     }
+    abstract public void loadAndSaveFiles(); // Paper - fix converting txt to json file; moved from DedicatedPlayerList constructor
 
     public void placeNewPlayer(Connection connection, ServerPlayer player, CommonListenerCookie clientData) {
         player.isRealPlayer = true; // Paper
