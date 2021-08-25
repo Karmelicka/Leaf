09a3db7f1eda7e8aa4c7b9539634b46589b4fade
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-08-25 13:19:53 -0700

    Vanilla command permission fixes
    Fixes permission checks for vanilla commands which don't have a
    requirement, as well as for namespaced vanilla commands.
    
    == AT ==
    public-f com.mojang.brigadier.tree.CommandNode requirement

--- src/main/java/net/minecraft/commands/Commands.java
+++ src/main/java/net/minecraft/commands/Commands.java
@@ -257,6 +257,13 @@ public class Commands {
         }
         this.vanillaCommandNodes.addAll(this.dispatcher.getRoot().getChildren()); // Paper - Add UnknownCommandEvent
 
+        // Paper start - Vanilla command permission fixes
+        for (final CommandNode<CommandSourceStack> node : this.dispatcher.getRoot().getChildren()) {
+            if (node.getRequirement() == com.mojang.brigadier.builder.ArgumentBuilder.<CommandSourceStack>defaultRequirement()) {
+                node.requirement = stack -> stack.source == CommandSource.NULL || stack.getBukkitSender().hasPermission(org.bukkit.craftbukkit.command.VanillaCommandWrapper.getPermission(node));
+            }
+        }
+        // Paper end - Vanilla command permission fixes
         // CraftBukkit start
     }
 
