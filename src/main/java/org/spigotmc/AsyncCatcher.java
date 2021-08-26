9bad7403c6a9d87d805a08eab55f9d3c85bd0d4d
Spottedleaf <Spottedleaf@users.noreply.github.com>
2021-08-25 20:17:12 -0700

    Improve and expand AsyncCatcher
    Log when the async catcher is tripped
      The chunk system can swallow the exception given it's all
      built with completablefuture, so ensure it is at least printed.
    
    Add/move several async catchers
    
    Async catch modifications to critical entity state
      These used to be here from Spigot, but were dropped with 1.17.
      Now in 1.17, this state is _even more_ critical than it was before,
      so these must exist to catch stupid plugins.
    
    Co-authored-by: Jake Potrebic <jake.m.potrebic@gmail.com>

--- src/main/java/org/spigotmc/AsyncCatcher.java
+++ src/main/java/org/spigotmc/AsyncCatcher.java
@@ -11,6 +11,7 @@ public class AsyncCatcher
     {
         if ( (AsyncCatcher.enabled || io.papermc.paper.util.TickThread.STRICT_THREAD_CHECKS) && Thread.currentThread() != MinecraftServer.getServer().serverThread ) // Paper
         {
+            MinecraftServer.LOGGER.error("Thread " + Thread.currentThread().getName() + " failed main thread check: " + reason, new Throwable()); // Paper
             throw new IllegalStateException( "Asynchronous " + reason + "!" );
         }
     }
