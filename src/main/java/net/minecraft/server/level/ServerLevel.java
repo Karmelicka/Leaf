bd5c9f249a6e5cd24b4cf66c4818f6aec2433e5a
Noah van der Aa <ndvdaa@gmail.com>
2022-01-07 11:58:26 +0100

    Don't tick markers
    Fixes https://github.com/PaperMC/Paper/issues/7276 and https://github.com/PaperMC/Paper/issues/8118
    by using a config option that, when set to false, does not add markers to the entity
    tick list at all and ignores them in Spigot's activation range checks. The entity tick
    list is only used in the tick and tickPassenger methods, so we can safely not add the
    markers to it. When the config option is set to true, markers are ticked as normal.

--- src/main/java/net/minecraft/server/level/ServerLevel.java
+++ src/main/java/net/minecraft/server/level/ServerLevel.java
@@ -2235,6 +2235,7 @@ public class ServerLevel extends Level implements WorldGenLevel {
         }
 
         public void onTickingStart(Entity entity) {
+            if (entity instanceof net.minecraft.world.entity.Marker && !paperConfig().entities.markers.tick) return; // Paper - Configurable marker ticking
             ServerLevel.this.entityTickList.add(entity);
         }
 
