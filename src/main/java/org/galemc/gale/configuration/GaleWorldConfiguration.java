8b6b7567a2ddda28c0ae575946c9ba640e5993e0
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2022-11-06 04:32:27 -0500

    Make tripwire duping fix configurable
    License: MIT (https://opensource.org/licenses/MIT)
    Leaf - https://github.com/Winds-Studio/Leaf

--- src/main/java/org/galemc/gale/configuration/GaleWorldConfiguration.java
+++ src/main/java/org/galemc/gale/configuration/GaleWorldConfiguration.java
@@ -96,6 +96,12 @@ public class GaleWorldConfiguration extends ConfigurationPart {
     public GameplayMechanics gameplayMechanics;
     public class GameplayMechanics extends ConfigurationPart {
 
+        public Fixes fixes;
+        public class Fixes extends ConfigurationPart {
+            public boolean tripwireDuping = true; // Gale - Leaf - make tripwire duping fix configurable
+
+        }
+
         public double entityWakeUpDurationRatioStandardDeviation = 0.2; // Gale - variable entity wake-up duration
         public boolean tryRespawnEnderDragonAfterEndCrystalPlace = true; // Gale - Pufferfish - make ender dragon respawn attempt after placing end crystals configurable
 
