9fed26d624b3d1a54d71c172afd8e54fd7e72f88
Martijn Muijsers <martijnmuijsers@live.nl>
2022-11-30 17:19:01 +0100

    Cache CubeVoxelShape shape array
    License: LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
    Gale - https://galemc.org
    
    This patch is based on the following mixin:
    "me/jellysquid/mods/lithium/mixin/shapes/precompute_shape_arrays/SimpleVoxelShapeMixin.java"
    By: Angeline <jellysquid3@users.noreply.github.com>
    As part of: Lithium (https://github.com/CaffeineMC/lithium-fabric)
    Licensed under: LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)

--- src/main/java/net/minecraft/world/phys/shapes/CubeVoxelShape.java
+++ src/main/java/net/minecraft/world/phys/shapes/CubeVoxelShape.java
@@ -3,8 +3,13 @@ package net.minecraft.world.phys.shapes;
 import it.unimi.dsi.fastutil.doubles.DoubleList;
 import net.minecraft.core.Direction;
 import net.minecraft.util.Mth;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public final class CubeVoxelShape extends VoxelShape {
+
+    private @NotNull DoubleList @Nullable [] list; // Gale - Lithium - cache CubeVoxelShape shape array
+
     protected CubeVoxelShape(DiscreteVoxelShape voxels) {
         super(voxels);
         this.initCache(); // Paper - optimise collisions
@@ -12,7 +17,15 @@ public final class CubeVoxelShape extends VoxelShape {
 
     @Override
     protected DoubleList getCoords(Direction.Axis axis) {
-        return new CubePointRange(this.shape.getSize(axis));
+        // Gale start - Lithium - cache CubeVoxelShape shape array
+        if (this.list == null) {
+            this.list = new DoubleList[Direction.Axis.VALUES.length];
+            for (Direction.Axis existingAxis : Direction.Axis.VALUES) {
+                this.list[existingAxis.ordinal()] = new CubePointRange(this.shape.getSize(axis));
+            }
+        }
+        return this.list[axis.ordinal()];
+        // Gale end - Lithium - cache CubeVoxelShape shape array
     }
 
     @Override
