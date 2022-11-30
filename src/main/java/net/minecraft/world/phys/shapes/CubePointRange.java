f186e9d0332ad6e8eb231da505264077a7f03374
Martijn Muijsers <martijnmuijsers@live.nl>
2022-11-30 17:28:37 +0100

    Replace parts by size in CubePointRange
    License: LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
    Gale - https://galemc.org

--- src/main/java/net/minecraft/world/phys/shapes/CubePointRange.java
+++ src/main/java/net/minecraft/world/phys/shapes/CubePointRange.java
@@ -3,14 +3,14 @@ package net.minecraft.world.phys.shapes;
 import it.unimi.dsi.fastutil.doubles.AbstractDoubleList;
 
 public class CubePointRange extends AbstractDoubleList {
-    private final int parts;
+    private final int size; // Gale - replace parts by size in CubePointRange
     private final double scale; // Gale - Lithium - replace division by multiplication in CubePointRange
 
     CubePointRange(int sectionCount) {
         if (sectionCount <= 0) {
             throw new IllegalArgumentException("Need at least 1 part");
         } else {
-            this.parts = sectionCount;
+            this.size = sectionCount + 1; // Gale - replace parts by size in CubePointRange
         }
         this.scale = 1.0D / sectionCount; // Gale - Lithium - replace division by multiplication in CubePointRange
     }
@@ -20,6 +20,6 @@ public class CubePointRange extends AbstractDoubleList {
     }
 
     public int size() {
-        return this.parts + 1;
+        return this.size; // Gale - replace parts by size in CubePointRange
     }
 }
