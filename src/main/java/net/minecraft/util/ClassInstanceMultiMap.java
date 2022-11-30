5a830fc4f14e24a26b2d0218448c07c14b1b3f62
Martijn Muijsers <martijnmuijsers@live.nl>
2022-11-30 21:02:43 +0100

    Replace class map with optimized collection
    License: LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
    Gale - https://galemc.org
    
    This patch is based on the following mixin:
    "me/jellysquid/mods/lithium/mixin/collections/entity_by_type/TypeFilterableListMixin.java"
    By: 2No2Name <2No2Name@web.de>
    As part of: Lithium (https://github.com/CaffeineMC/lithium-fabric)
    Licensed under: LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)

--- src/main/java/net/minecraft/util/ClassInstanceMultiMap.java
+++ src/main/java/net/minecraft/util/ClassInstanceMultiMap.java
@@ -4,6 +4,8 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterators;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap;
+
 import java.util.AbstractCollection;
 import java.util.Collection;
 import java.util.Collections;
@@ -13,7 +15,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ClassInstanceMultiMap<T> extends AbstractCollection<T> {
-    private final Map<Class<?>, List<T>> byClass = Maps.newHashMap();
+    private final Map<Class<?>, List<T>> byClass = new Reference2ReferenceOpenHashMap<>(2); // Gale - Lithium - replace class map with optimized collection
     private final Class<T> baseClass;
     private final List<T> allInstances = Lists.newArrayList();
 
